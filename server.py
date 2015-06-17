29a3d30b1a1aa10a0c1a1d4482e3af1bccc2cb29
伊藤 吉弥 <myon53@gmail.com>
2015-06-18 00:41:35 +0900

    refactoring using contextmanager

--- server.py
+++ server.py
@@ -11,16 +11,42 @@ import asyncio
 import json
 import pymongo as m
 import rethinkdb as r
-from time import time
-from tornado.platform.asyncio import AsyncIOMainLoop
 import tornado.httpserver
 import tornado.httpclient
 import tornado.ioloop
 import tornado.options
 import tornado.web
+from tornado.platform.asyncio import AsyncIOMainLoop
 from tornado.options import define, options
+from contextlib import contextmanager
+from time import time
 
 define('port', default=8000, help='this is help', type=int)
+
+
+class Chronos(object):
+
+    def __init__(self, *, title='', start=None, end=None):
+        '''
+        args: start and end should not set time() as default
+        '''
+        self.title = title
+        self.start = start
+        self.end = end
+
+    def duration(self):
+        return (self.end - self.start)
+
+    def __str__(self):
+        return self.title + ': ' + str(self.duration())
+
+    @contextmanager
+    def __call__(self):
+        try:
+            self.start = time()
+            yield
+        finally:
+            self.end = time()
 
 
 class SyncBenchMongoDB(tornado.web.RequestHandler):
@@ -43,41 +69,38 @@ class SyncBenchMongoDB(tornado.web.RequestHandler):
         return self.write(res_json)
 
     def bulk_insert_massive(self):
-        start = time()
-        bench = self.db.bench
-        data = [{'_id': i, 'name': ('name' + str(i))} for i in range(0, 10000)]
-        bench.insert_many(data)
-        end = time()
-        diff = end - start
-        print('bulk_insert_massive in pymongo: ', str(diff))
-        return diff
+        chronos = Chronos(title='bulk_insert_massive in pymongo')
+        with chronos():
+            bench = self.db.bench
+            data = [{'_id': i, 'name': ('name' + str(i))} \
+                    for i in range(0, 10000)]
+            bench.insert_many(data)
+        print(chronos)
+        return chronos.duration()
 
     def find_massive(self):
-        start = time()
-        bench = self.db.bench
-        _ = [doc for doc in bench.find({})]
-        end = time()
-        diff = end - start
-        print('find_massive in pymongo: ', str(diff))
-        return diff
+        chronos = Chronos(title='find_massive in pymongo')
+        with chronos():
+            bench = self.db.bench
+            _ = [doc for doc in bench.find({})]
+        print(chronos)
+        return chronos.duration()
 
     def find_one(self):
-        start = time()
-        bench = self.db.bench
-        bench.find_one({'_id': 5555})
-        end = time()
-        diff = end - start
-        print('find_one in pymongo: ', str(diff))
-        return diff
+        chronos = Chronos(title='find_one in pymongo')
+        with chronos():
+            bench = self.db.bench
+            bench.find_one({'_id': 5555})
+        print(chronos)
+        return chronos.duration()
 
     def delete_massive(self):
-        start = time()
-        bench = self.db.bench
-        bench.drop()
-        end = time()
-        diff = end - start
-        print('delete_massive in pymongo: ', str(diff))
-        return diff
+        chronos = Chronos(title='delete_massive in pymongo')
+        with chronos():
+            bench = self.db.bench
+            bench.drop()
+        print(chronos)
+        return chronos.duration()
 
 
 class SyncBenchRethinkDB(tornado.web.RequestHandler):
